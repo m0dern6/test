4b04b2b48109d1a3bfa908f527d39cba4cda4ce0
m0dern6 <na.ab.en.153@gmail.com>
2024-08-03 23:15:15 +0545

    fix 3

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -25,7 +25,7 @@ class _HomeState extends State<Home> {
               Row(
                 children: [
                   Text(
-                    'Hi ',
+                    'Hi Hi',
                     style: TextStyle(
                       fontSize: _fontSize,
                       fontWeight: FontWeight.bold,
