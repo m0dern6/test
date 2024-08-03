0c111b851c6b5e312128ec2b34e518dfa9342e1a
m0dern6 <na.ab.en.153@gmail.com>
2024-08-03 20:22:25 +0545

    changed a text

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -25,7 +25,7 @@ class _HomeState extends State<Home> {
               Row(
                 children: [
                   Text(
-                    'Hello ',
+                    'Hello Hello',
                     style: TextStyle(
                       fontSize: _fontSize,
                       fontWeight: FontWeight.bold,
