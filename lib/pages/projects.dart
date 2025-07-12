d26cbbe787c7e24459504d3b74706bdeb6667a0b
m0dern6 <na.ab.en.153@gmail.com>
2025-07-12 19:22:14 +0545

    complete portfoilio web app

--- lib/pages/projects.dart
+++ lib/pages/projects.dart
@@ -162,7 +162,7 @@ class _ProjectsState extends State<Projects> with TickerProviderStateMixin {
                   child: Center(
                     child: Text(
                       '${categories[index]} Projects',
-                      textAlign: TextAlign.center,
+                      textAlign: TextAlign.cente,
                       style: GoogleFonts.dmSans(
                         fontSize: isMobile ? 12 : 16,
                         fontWeight: FontWeight.w600,
